6f12719aa037be3e9fc4c556d4e9165f7467c9c8
Kamil Krzysztof <kamil@Mac-mini-Kamil.home>
2020-08-07 21:32:46 +0200

    Initial, pageviewer added

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ViewPager from '@react-native-community/viewpager';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.navbar}>
+        <Text>NavBar</Text>
+      </View>
+      <ViewPager style={{flex:6}} initialPage={0}>
+      <Text>zero page</Text>
+      <View key="1">
+        <Text>First page</Text>
+      </View>
+      <View key="2">
+        <Text>Second page</Text>
+      </View>
+    </ViewPager>
     </View>
   );
 }
@@ -15,7 +27,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  navbar: {
+    flex: 1,
+    backgroundColor: 'blue',
+
+  },
+  content: {
+    flex: 6,
+    backgroundColor: 'red',
+
+  }
 });
